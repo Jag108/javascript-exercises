c6603f8eed50173a2133bf80803d21d60174a845
jag108 <251528128+Jag108@users.noreply.github.com>
2026-01-31 09:37:16 +0800

    Add condition to return error if num is negative

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,8 +1,14 @@
 const repeatString = function(str, num) {
     
+    
+    if(0 > num){
+        return str = 'ERROR';
+    }
+    
     let repeated = "";
     
     for(let i = 1; i <= num; ++i){
+
         repeated += str;
     }
     return repeated;
