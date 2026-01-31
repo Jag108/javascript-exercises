3da4e9c0b82e421c5eda53d92202dd865c03f576
jag108 <251528128+Jag108@users.noreply.github.com>
2026-01-31 18:03:11 +0800

    Create function that sums every integer between 2 positive integers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let start = 0;
+    let end = 0;
+    let total = 0;
 
+    if(num1 < num2){
+        start = num1;
+        end = num2;
+    }else{
+        start = num2;
+        end = num1;
+    }
+
+    for(start; start <= end; ++start){
+        total += start;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
