76cc7c7e1bceb236429e8e93a74c22a9378220bb
jag108 <251528128+Jag108@users.noreply.github.com>
2026-01-31 10:59:26 +0800

    Create function to remove array with given arg

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    let removedArr = [];
+    for(let i = 0; i < array.length - 1; ++i){
+        for(const arg of numbers){
+            if(array[i] === arg){
+               removedArr = array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
